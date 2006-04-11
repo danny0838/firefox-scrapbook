d561097892adf6d002685a848d3b69171e33784b
Gomita <gomita@xuldev.org>
2006-04-11 16:50:36 +0000

    (extracted chrome/scrapbook.jar to chrome/) Scrapbook 1.0.1
    (cherry picked from commit e27d0e746d48ba347ceb01853109840db3414b1b)

--- chrome/content/scrapbook/about.js
+++ chrome/content/scrapbook/about.js
@@ -1,6 +1,6 @@
 
-const kVERSION = "1.0";
-const kBUILD_TEXT = " (Build ID 20060410)";
+const kVERSION = "1.0.1";
+const kBUILD_TEXT = " (Build ID 20060411)";
 const kUPDATE_URL = "http://amb.vis.ne.jp/mozilla/scrapbook/update.rdf";
 
 var gAboutString;
