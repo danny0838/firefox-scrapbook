4468149b06b7713e3244ad82cfacee625b546329
Gomita <gomita@xuldev.org>
2006-09-11 15:05:11 +0000

    (extracted chrome/scrapbook.jar to chrome/) Scrapbook 1.2.0.3
    Fixed a locale specified critical bug: [Browse] button in [Settings] doesn't work.
    
    (cherry picked from commit e734303f6d62248558a83550a6b32dfe11d92460)

--- chrome/content/scrapbook/about.js
+++ chrome/content/scrapbook/about.js
@@ -1,6 +1,6 @@
 
-const kVERSION = "1.2.0.2";
-const kBUILD_TEXT = " (Build ID 20060910)";
+const kVERSION = "1.2.0.3";
+const kBUILD_TEXT = " (Build ID 20060911)";
 const kUPDATE_URL = "http://amb.vis.ne.jp/mozilla/scrapbook/update.rdf";
 
 var gAboutString;
