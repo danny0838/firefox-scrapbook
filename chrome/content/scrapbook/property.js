7c7e334fd63c380b75befa55ed2e4ea03e433f1f
Danny Lin <danny0838@gmail.com>
2014-06-08 19:46:52 +0800

    File size calculation now looks into sub-directories. Fixes an issue that unreadable files (possibility due to improper file name) causes calculation to freeze.

--- chrome/content/scrapbook/property.js
+++ chrome/content/scrapbook/property.js
@@ -185,14 +185,25 @@ var sbPropService = {
 	{
 		var totalSize = 0;
 		var totalFile = 0;
-		var dir = ScrapBookUtils.getContentDir(aID, true);
-		if ( !dir || !dir.isDirectory() ) return [0, 0];
-		var fileEnum = dir.directoryEntries;
-		while ( fileEnum.hasMoreElements() )
-		{
-			var file = fileEnum.getNext().QueryInterface(Ci.nsIFile);
-			totalSize += file.fileSize;
-			totalFile++;
+		var dir = [ScrapBookUtils.getContentDir(aID, true)];
+		for (var i=0; i<dir.length; i++) {
+			var fileEnum = dir[i].directoryEntries;
+			while ( fileEnum.hasMoreElements() )
+			{
+				try {
+					var file = fileEnum.getNext().QueryInterface(Ci.nsIFile);
+					if (file.isDirectory()) {
+						dir.push(file);
+					}
+					else {
+						totalSize += file.fileSize;
+						totalFile++;
+					}
+				}
+				catch (ex) {
+					alert("ERROR: cannot read file size (possibility due to improper file name): " + file.path);
+				}
+			}
 		}
 		return [totalSize, totalFile];
 	},
