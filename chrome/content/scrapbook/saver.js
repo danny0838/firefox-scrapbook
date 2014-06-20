9c7c549943cc81e4d72029ddd075bc461b2d1f80
Danny Lin <danny0838@gmail.com>
2014-06-20 11:54:16 +0800

    Fix an issue that links of undownloadable files are corrupted in the CSS.

--- chrome/content/scrapbook/saver.js
+++ chrome/content/scrapbook/saver.js
@@ -696,25 +696,23 @@ var sbContentSaver = {
 		return content;
 	},
 
-	inspectCSSText : function(aCSStext, aCSShref, isImage)
+	inspectCSSText : function(aCSSText, aCSSHref, isImage)
 	{
-		if (!aCSShref) aCSShref = this.refURLObj.spec;
+		if (!aCSSHref) aCSSHref = this.refURLObj.spec;
 		// CSS get by .cssText is always url("something-with-\"double-quote\"-escaped")
 		// and no CSS comment is in
 		// so we can parse it safely with this RegExp
-		aCSStext = aCSStext.replace(/ url\(\"((?:\\.|[^"])+)\"\)/g, function() {
+		aCSSText = aCSSText.replace(/ url\(\"((?:\\.|[^"])+)\"\)/g, function() {
 			var dataURL = arguments[1];
 			if (dataURL.indexOf("data:") === 0) return ' url("' + dataURL + '")';
-			dataURL = sbCommonUtils.resolveURL(aCSShref, dataURL);
-			if (isImage) {
-				var dataFile = sbContentSaver.option["images"] ? sbContentSaver.download(dataURL) : dataURL;
-			}
-			else {
+			dataURL = sbCommonUtils.resolveURL(aCSSHref, dataURL);
+			if (sbContentSaver.option["images"] || !isImage) {
 				var dataFile = sbContentSaver.download(dataURL);
+				if (dataFile) dataURL = dataFile;
 			}
-			return ' url("' + dataFile + '")';
+			return ' url("' + dataURL + '")';
 		});
-		return aCSStext;
+		return aCSSText;
 	},
 
 	download : function(aURLSpec)
