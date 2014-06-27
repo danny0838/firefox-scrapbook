65cdb33274bc107abad1fc93441f4ab9027ebc65
Danny Lin <danny0838@gmail.com>
2014-06-27 20:06:02 +0800

    Add folder count.

--- chrome/content/scrapbook/property.js
+++ chrome/content/scrapbook/property.js
@@ -70,7 +70,7 @@ var sbPropService = {
 	delayedInit : function()
 	{
 		var sizeCount = this.getTotalFileSize(this.id);
-		document.getElementById("sbPropSize").value = sbCommonUtils.lang("property", "FILES_COUNT", [sbPropService.formatFileSize(sizeCount[0]), sizeCount[1]]);
+		document.getElementById("sbPropSize").value = sbCommonUtils.lang("property", "FILES_COUNT", [sbPropService.formatFileSize(sizeCount[0]), sizeCount[1], sizeCount[2]]);
 	},
 
 	accept : function()
@@ -184,7 +184,11 @@ var sbPropService = {
 	{
 		var totalSize = 0;
 		var totalFile = 0;
+		var totalDir  = 0;
 		sbCommonUtils.forEachFile(sbCommonUtils.getContentDir(aID, true), function(){
+			if (this.isDirectory()) {
+				totalDir++;
+			}
 			if (!this.isFile()) return;
 			try {
 				totalSize += this.fileSize;
@@ -194,7 +198,7 @@ var sbPropService = {
 			    alert(sbCommonUtils.lang("scrapbook", "ERR_FAIL_READ_FILE_SIZE", [this.path]));
 			}
 		});
-		return [totalSize, totalFile];
+		return [totalSize, totalFile, totalDir];
 	},
 
 	formatFileSize : function(aBytes)
