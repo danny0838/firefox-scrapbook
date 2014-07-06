bbf575f81a32834b6de64b626f75d948b30d5858
Danny Lin <danny0838@gmail.com>
2014-07-06 18:46:27 +0800

    Fix: Remove loose detection for getID to prevent potential problems. Only files under current activate ScrapBook are regarded editable.
    - eg. "file:///somedir/data/43210987654321/scrapbook/data/12345678901234" is ambiguous, in previous version we get id "43210987654321" while it could be "12345678901234" if the scrapbook dir is "file:///somedir/data/43210987654321/scrapbook"

--- chrome/content/scrapbook/overlay.js
+++ chrome/content/scrapbook/overlay.js
@@ -132,8 +132,9 @@ var sbBrowserOverlay = {
 		var file = sbCommonUtils.convertURLToFile(aURL);
 		if (!file || !file.exists() || !file.isFile()) return null;
 		var aURL = sbCommonUtils.convertFilePathToURL(file.path);
-		var editable = (aURL.indexOf("file:") == 0 && aURL.match(/\/data\/(\d{14})\//));
-		return editable ? RegExp.$1 : null;
+		var sbDir = sbCommonUtils.convertFilePathToURL(sbCommonUtils.getScrapBookDir().path);
+		var sbPath = new RegExp("^" + sbCommonUtils.escapeRegExp(sbDir) + "data/(\\d{14})/");
+		return aURL.match(sbPath) ? RegExp.$1 : null;
 	},
 
 	onLocationChange: function(aURL)
