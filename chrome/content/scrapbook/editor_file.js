8347c717a8a1a642a6a8492b193399a58399e31f
Danny Lin <danny0838@gmail.com>
2014-07-14 10:12:07 +0800

    Persist last insert type of insert link and insert file dialog with pref.

--- chrome/content/scrapbook/editor_file.js
+++ chrome/content/scrapbook/editor_file.js
@@ -4,6 +4,7 @@ function init() {
 	if ( !window.arguments) window.close();
 	gData = window.arguments[0];
 	if (gData.url) document.getElementById("sbFileHTML").value = gData.url;
+	pick( sbCommonUtils.getPref("edit.file.lastType", "sbFileFileUse") );
 	document.getElementById("sbFileInsert").checked = sbCommonUtils.getPref("edit.file.lastInsert", true);
 	var lastFormat = sbCommonUtils.getPref("edit.file.lastFormat", "");
 	if (lastFormat) document.getElementById("sbFileFormat").value = lastFormat;
@@ -17,6 +18,7 @@ function accept() {
 	gData.insert = document.getElementById("sbFileInsert").checked;
 	gData.format = document.getElementById("sbFileFormat").value;
 	gData.result = ((gData.file_use && gData.file) || (gData.html_use && gData.html)) ? 1 : 0;
+	sbCommonUtils.setPref("edit.file.lastType", gData.file_use ? "sbFileFileUse" : "sbFileHTMLUse");
 	sbCommonUtils.setPref("edit.file.lastInsert", gData.insert);
 	sbCommonUtils.setPref("edit.file.lastFormat", gData.format);
 }
