fc3116a4edf7975210f4fa6773b00d227297de34
Danny Lin <danny0838@gmail.com>
2014-08-06 00:13:59 +0800

    Fix: use document title for MB_CLOSE_WINDOW alert.

--- chrome/content/scrapbook/mbService.js
+++ chrome/content/scrapbook/mbService.js
@@ -101,7 +101,7 @@ var sbMultiBookService = {
 			var win = winEnum.getNext().QueryInterface(Components.interfaces.nsIDOMWindow);
 			if (win != window) {
 				if (!aQuietWarning)
-					alert(sbCommonUtils.lang("scrapbook", "MB_CLOSE_WINDOW", [win.title]));
+					alert(sbCommonUtils.lang("scrapbook", "MB_CLOSE_WINDOW", [win.document.title]));
 				return false;
 			}
 		}
