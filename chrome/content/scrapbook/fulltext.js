af58dcdcd7ea194b738faef8d34927945b45c266
Danny Lin <danny0838@gmail.com>
2014-08-20 14:54:00 +0800

    Fulltext now indexes bookmarks.

--- chrome/content/scrapbook/fulltext.js
+++ chrome/content/scrapbook/fulltext.js
@@ -262,16 +262,27 @@ var sbSearchResult =
 	{
 		if ( !this.CURRENT_TREEITEM ) return;
 		var id   = this.CURRENT_TREEITEM[5];
-		var url  = this.CURRENT_TREEITEM[6] == "note" ? "chrome://scrapbook/content/note.xul?id=" + id : this.resPathToURL(id, this.CURRENT_TREEITEM[4]);
 		switch ( key ) {
-			case "O" : sbCommonUtils.loadURL(url, false); break;
-			case "T" : sbCommonUtils.loadURL(url, true); break;
+			case "O" : sbCommonUtils.loadURL(getURL(), false); break;
+			case "T" : sbCommonUtils.loadURL(getURL(), true); break;
 			case "P" : window.openDialog("chrome://scrapbook/content/property.xul", "", "modal,centerscreen,chrome" ,id); break;
 			case "L" : 
-				var res = sbCommonUtils.RDF.GetResource("urn:scrapbook:item" + sbSearchResult.CURRENT_TREEITEM[5]);
+				var res = sbCommonUtils.RDF.GetResource("urn:scrapbook:item" + id);
 				sbCommonUtils.WINDOW.getMostRecentWindow("navigator:browser").sbBrowserOverlay.execLocate(res);
 				break;
-			default  : document.getElementById("sbBrowser").loadURI(url); break;
+			default  : document.getElementById("sbBrowser").loadURI(getURL()); break;
+		}
+		
+		function getURL() {
+			switch (sbSearchResult.CURRENT_TREEITEM[6]) {
+				case "note":
+					return "chrome://scrapbook/content/note.xul?id=" + id;
+				case "bookmark":
+					var res = sbCommonUtils.RDF.GetResource("urn:scrapbook:item" + id);
+					return sbDataSource.getProperty(res, "source");
+				default:
+					return sbSearchResult.resPathToURL(id, sbSearchResult.CURRENT_TREEITEM[4]);
+			}
 		}
 	},
 
@@ -419,7 +430,7 @@ var sbCacheService = {
 				}
 				break;
 			case "bookmark":
-				
+				sbCacheService.inspectFile(null, "index.html", "none");
 				break;
 			default:
 				sbCommonUtils.error(sbCommonUtils.lang("scrapbook", "ERR_UNKNOWN_DATA_TYPE", [type]));
@@ -438,22 +449,27 @@ var sbCacheService = {
 	{
 		var resource = sbCommonUtils.RDF.GetResource(this.resList[this.index].ValueUTF8 + "#" + aSubPath);
 		var charset = sbDataSource.getProperty(sbCacheService.resList[sbCacheService.index], "chars");
-		// if cache is newer, skip caching this file and its frames
-		// (only check update of the main page)
-		if ( sbCacheSource.exists(resource) ) {
-			if ( gCacheFile.lastModifiedTime > aFile.lastModifiedTime && charset == sbCacheSource.getProperty(resource, "charset") )
-			{
-				if (mode == "html") sbCacheService.checkFrameFiles(aFile, function(){return 0;});
-				this.uriHash[resource.ValueUTF8] = true;
-				sbCacheSource.updateEntry(resource, "folder",  this.folders[this.index]);
-				return;
+		if (aFile) {
+			// if cache is newer, skip caching this file and its frames
+			// (only check update of the main page)
+			if ( sbCacheSource.exists(resource) ) {
+				if ( gCacheFile.lastModifiedTime > aFile.lastModifiedTime && charset == sbCacheSource.getProperty(resource, "charset") )
+				{
+					if (mode == "html") sbCacheService.checkFrameFiles(aFile, function(){return 0;});
+					this.uriHash[resource.ValueUTF8] = true;
+					sbCacheSource.updateEntry(resource, "folder",  this.folders[this.index]);
+					return;
+				}
 			}
+			// cache text in the file and its frames
+			var contents = [];
+			addContent(aFile);
+			if (mode == "html") sbCacheService.checkFrameFiles(aFile, addContent);
+			contents = contents.join("\t").replace(/[\x00-\x1F\x7F]/g, " ").replace(/\s+/g, " ");
 		}
-		// cache text in the file and its frames
-		var contents = [];
-		addContent(aFile);
-		if (mode == "html") sbCacheService.checkFrameFiles(aFile, addContent);
-		contents = contents.join("\t").replace(/[\x00-\x1F\x7F]/g, " ").replace(/\s+/g, " ");
+		else {
+			var contents = "";
+		}
 		// update cache data
 		if ( sbCacheSource.exists(resource) ) {
 			sbCacheSource.updateEntry(resource, "folder",  this.folders[this.index]);
