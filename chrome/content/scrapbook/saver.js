8be96b8f156924757c70fb2c8b3eb20332a888d2
Danny Lin <danny0838@gmail.com>
2014-10-19 21:23:26 +0800

    Redefine defalut "Images", "Media", and "Fonts" behavior back to remove non-capture objects. Add "keepLink" option for the original behavior.

--- chrome/content/scrapbook/saver.js
+++ chrome/content/scrapbook/saver.js
@@ -25,7 +25,7 @@ var sbContentSaver = {
 		this.name = "index";
 		this.favicon = null;
 		this.file2URL = { "index.dat" : true, "index.png" : true, "sitemap.xml" : true, "sb-file2url.txt" : true, "sb-url2name.txt" : true, };
-		this.option   = { "dlimg" : false, "dlsnd" : false, "dlmov" : false, "dlarc" : false, "custom" : "", "inDepth" : 0, "isPartial" : false, "images" : true, "media" : true, "fonts" : true, "styles" : true, "script" : false, "asHtml" : false, "forceUtf8" : true, "rewriteStyles" : true, "internalize" : false };
+		this.option   = { "dlimg" : false, "dlsnd" : false, "dlmov" : false, "dlarc" : false, "custom" : "", "inDepth" : 0, "isPartial" : false, "images" : true, "media" : true, "fonts" : true, "styles" : true, "script" : false, "asHtml" : false, "forceUtf8" : true, "rewriteStyles" : true, "keepLink" : false, "internalize" : false };
 		this.plusoption = { "timeout" : "0", "charset" : "UTF-8" }
 		this.linkURLs = [];
 		this.frames = [];
@@ -433,8 +433,10 @@ var sbContentSaver = {
 					if ( this.option["images"] ) {
 						var aFileName = this.download(aNode.src);
 						if (aFileName) aNode.setAttribute("src", sbCommonUtils.escapeFileName(aFileName));
-					} else {
+					} else if ( this.option["keepLink"] ) {
 						aNode.setAttribute("src", aNode.src);
+					} else {
+						return this.removeNodeFromParent(aNode);
 					}
 				}
 				break;
@@ -445,8 +447,10 @@ var sbContentSaver = {
 					if ( this.option["media"] ) {
 						var aFileName = this.download(aNode.src);
 						if (aFileName) aNode.setAttribute("src", sbCommonUtils.escapeFileName(aFileName));
-					} else {
+					} else if ( this.option["keepLink"] ) {
 						aNode.setAttribute("src", aNode.src);
+					} else {
+						return this.removeNodeFromParent(aNode);
 					}
 				}
 				break;
@@ -456,8 +460,10 @@ var sbContentSaver = {
 					if ( this.option["media"] ) {
 						var aFileName = this.download(aNode.data);
 						if (aFileName) aNode.setAttribute("data", sbCommonUtils.escapeFileName(aFileName));
-					} else {
+					} else if ( this.option["keepLink"] ) {
 						aNode.setAttribute("data", aNode.src);
+					} else {
+						return this.removeNodeFromParent(aNode);
 					}
 				}
 				break;
@@ -468,8 +474,10 @@ var sbContentSaver = {
 					if ( this.option["media"] ) {
 						var aFileName = this.download(url);
 						if (aFileName) aNode.setAttribute("archive", sbCommonUtils.escapeFileName(aFileName));
-					} else {
+					} else if ( this.option["keepLink"] ) {
 						aNode.setAttribute("archive", url);
+					} else {
+						return this.removeNodeFromParent(aNode);
 					}
 				}
 				break;
@@ -491,8 +499,10 @@ var sbContentSaver = {
 					if ( this.option["images"] ) {
 						var aFileName = this.download(url);
 						if (aFileName) aNode.setAttribute("background", sbCommonUtils.escapeFileName(aFileName));
-					} else {
+					} else if ( this.option["keepLink"] ) {
 						aNode.setAttribute("background", url);
+					} else {
+						aNode.removeAttribute("background");
 					}
 				}
 				break;
@@ -504,8 +514,10 @@ var sbContentSaver = {
 							if ( this.option["images"] ) {
 								var aFileName = this.download(aNode.src);
 								if (aFileName) aNode.setAttribute("src", sbCommonUtils.escapeFileName(aFileName));
-							} else {
+							} else if ( this.option["keepLink"] ) {
 								aNode.setAttribute("src", aNode.src);
+							} else {
+								aNode.removeAttribute("src");
 							}
 						}
 						break;
@@ -841,12 +853,16 @@ var sbContentSaver = {
 					if (sbContentSaver.option["images"]) {
 						var dataFile = sbContentSaver.download(dataURL);
 						if (dataFile) dataURL = sbCommonUtils.escapeHTML(sbCommonUtils.escapeFileName(dataFile));
+					} else if (!sbContentSaver.option["keepLink"]) {
+						dataURL = "about:blank";
 					}
 					break;
 				case "font":
 					if (sbContentSaver.option["fonts"]) {
 						var dataFile = sbContentSaver.download(dataURL);
 						if (dataFile) dataURL = sbCommonUtils.escapeHTML(sbCommonUtils.escapeFileName(dataFile));
+					} else if (!sbContentSaver.option["keepLink"]) {
+						dataURL = "about:blank";
 					}
 					break;
 			}
