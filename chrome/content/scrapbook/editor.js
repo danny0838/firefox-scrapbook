a62687cc70785758a62ad013e43e73f8b8b6cde5
Danny Lin <danny0838@gmail.com>
2014-11-16 22:19:43 +0800

    Add overflow definition to mainDiv. Also, bodyDiv uses overflow visible so that the editing view more corresponds to the result view.

--- chrome/content/scrapbook/editor.js
+++ chrome/content/scrapbook/editor.js
@@ -1946,6 +1946,7 @@ var sbAnnotationService = {
 		// create a new freenote
 		var mainDiv = win.content.document.createElement("DIV");
 		mainDiv.setAttribute("data-sb-obj", "freenote");
+		mainDiv.style.overflow = "visible";
 		mainDiv.style.margin = "0px";
 		mainDiv.style.border = "1px solid #CCCCCC";
 		mainDiv.style.borderTopWidth = (this.FREENOTE_HEADER_HEIGHT + 1) + "px";
@@ -2040,7 +2041,7 @@ var sbAnnotationService = {
 		bodyDiv.innerHTML = mainDiv.innerHTML;
 		bodyDiv.setAttribute("contentEditable", true);
 		bodyDiv.style.cursor = "auto";
-		bodyDiv.style.overflow = "auto";
+		bodyDiv.style.overflow = "visible";
 		bodyDiv.style.margin = "0px";
 		bodyDiv.style.border = "none";
 		bodyDiv.style.background = "#FFFFFF";
