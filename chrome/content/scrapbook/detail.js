06622839c9d45227ce881e08dc08f0cbac6d3c7a
Danny Lin <danny0838@gmail.com>
2016-08-12 04:31:23 +0800

    capture: remove rewriteStyles option from GUI

--- chrome/content/scrapbook/detail.js
+++ chrome/content/scrapbook/detail.js
@@ -14,7 +14,6 @@ var sbCaptureOptions = {
         document.getElementById("sbDetailOptionStyles").checked = sbCommonUtils.getPref("capture.default.styles", true);
         document.getElementById("sbDetailOptionScript").checked = sbCommonUtils.getPref("capture.default.script", false);
         document.getElementById("sbDetailOptionAsHtml").checked = sbCommonUtils.getPref("capture.default.asHtml", false);
-        document.getElementById("sbDetailOptionRewriteStyles").checked = sbCommonUtils.getPref("capture.default.rewriteStyles", true);
         document.getElementById("sbDetailOptionKeepLink").checked = sbCommonUtils.getPref("capture.default.keepLink", false);
         document.getElementById("sbDetailOptionSaveDataURI").checked = sbCommonUtils.getPref("capture.default.saveDataURI", false);
         document.getElementById("sbDetailDownLinkMethod").value = sbCommonUtils.getPref("capture.default.downLinkMethod", 0);
@@ -82,7 +81,6 @@ var sbCaptureOptions = {
         this.param.option["styles"] = document.getElementById("sbDetailOptionStyles").checked;
         this.param.option["script"] = document.getElementById("sbDetailOptionScript").checked;
         this.param.option["asHtml"] = document.getElementById("sbDetailOptionAsHtml").checked;
-        this.param.option["rewriteStyles"] = document.getElementById("sbDetailOptionRewriteStyles").checked;
         this.param.option["keepLink"] = document.getElementById("sbDetailOptionKeepLink").checked;
         this.param.option["saveDataURI"] = document.getElementById("sbDetailOptionSaveDataURI").checked;
         this.param.option["downLinkMethod"] = parseInt("0" + document.getElementById("sbDetailDownLinkMethod").value, 10);
@@ -98,7 +96,6 @@ var sbCaptureOptions = {
         sbCommonUtils.setPref("capture.default.styles", this.param.option["styles"]);
         sbCommonUtils.setPref("capture.default.script", this.param.option["script"]);
         sbCommonUtils.setPref("capture.default.asHtml", this.param.option["asHtml"]);
-        sbCommonUtils.setPref("capture.default.rewriteStyles", this.param.option["rewriteStyles"]);
         sbCommonUtils.setPref("capture.default.keepLink", this.param.option["keepLink"]);
         sbCommonUtils.setPref("capture.default.saveDataURI", this.param.option["saveDataURI"]);
         sbCommonUtils.setPref("capture.default.downLinkMethod", this.param.option["downLinkMethod"]);
